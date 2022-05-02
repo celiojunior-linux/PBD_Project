1699dc903cdab04fefaa850280663e3abb6a722d
Célio Júnior <cj2018lima@gmail.com>
2022-05-02 06:50:39 -0300

    Added base template

--- apps/service/urls.py
+++ apps/service/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from apps.service import views
 
 urlpatterns = [
-    path('create/', views.service_order_crete_view, name="service-create"),
+    path('create/', views.BaseTemplate.as_view(), name="service-create"),
 ]
--- apps/service/views.py
+++ apps/service/views.py
@@ -1,1 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class BaseTemplate(TemplateView):
+    template_name = "service/base_template.html"
