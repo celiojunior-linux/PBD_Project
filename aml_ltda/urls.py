6f5c5cb6771ae0d48cd1d91e13dc541072a66316
Célio Júnior <celio@layoutsistemas.com.br>
2022-05-02 16:40:57 -0300

    Configured media url

--- aml_ltda/urls.py
+++ aml_ltda/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('service/', include('apps.service.urls'))
 ]
+
+if settings.DEBUG:
+    from django.templatetags.static import static
+    urlpatterns += static(settings.MEDIA_URL)
