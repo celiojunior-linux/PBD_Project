7c92076b96b1488a87a947bd4be1d90a5e974405
Célio Júnior <celio@layoutsistemas.com.br>
2022-05-02 17:08:18 -0300

    Implemented service models

--- apps/service/models.py
+++ apps/service/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    code = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=100)
+    total_cost = models.FloatField(editable=False, default=0)
+
+
+class ServiceItem(models.Model):
+    service = models.ForeignKey(to=Service, on_delete=models.CASCADE)
+    code = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=100)
+    cost = models.FloatField()
+    
