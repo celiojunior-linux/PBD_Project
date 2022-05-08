d1f8f603412e555f669f2dabee6369ce47fdb1f5
Célio Júnior <cj2018lima@gmail.com>
2022-05-08 07:55:57 -0300

    Changed URL

--- aml_ltda/settings.py
+++ aml_ltda/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['pdb-project.herokuapp.com', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['auto-mecanica-pbd', '0.0.0.0', 'localhost']
 
 # Application definition
 
