26d108e7d53cfcb09010704f93faf82da98d35b1
Célio Júnior <cj2018lima@gmail.com>
2022-05-08 11:19:34 -0300

    Changed captcha

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -6,9 +6,8 @@ from django.conf import settings
 
 class AMLAuthenticationForm(AuthenticationForm):
 
-    if settings.DEBUG is False:
-        captcha = ReCaptchaField(
-            public_key=settings.RECAPTCHA_PUBLIC_KEY,
-            private_key=settings.RECAPTCHA_PRIVATE_KEY,
-            widget=ReCaptchaV2Checkbox()
-        )
+    captcha = ReCaptchaField(g
+        public_key=settings.RECAPTCHA_PUBLIC_KEY,
+        private_key=settings.RECAPTCHA_PRIVATE_KEY,
+        widget=ReCaptchaV2Checkbox()
+    )
