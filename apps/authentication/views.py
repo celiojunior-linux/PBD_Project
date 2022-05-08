2c21e735181f7d98363a030a64c61073c0d3adc4
Célio Júnior <cj2018lima@gmail.com>
2022-05-08 09:06:29 -0300

    Updated recaptcha settings

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -4,9 +4,12 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponseRedirect
 
+from apps.authentication.forms import AMLAuthenticationForm
+
 
 class AMLLoginView(LoginView):
     template_name = "auth/login.html"
+    form_class = AMLAuthenticationForm
 
     def form_valid(self, form):
         employee = authenticate(
